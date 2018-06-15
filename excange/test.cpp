1f75c3033c81ef92725736a3c2267f6f0597a7ed
ofhuji_masa <ofhuji_masa@yahoo.co.jp>
2018-06-16 00:22:55 +0900

    test2

--- excange/test.cpp
+++ excange/test.cpp
@@ -1,25 +1,33 @@
 #include <iostream>
-#include <string>
+#include <list>
+ 
 using namespace std;
 
-class masa{
+class masa {
 private:
+	string name;
 	int age;
 public:
-	masa(int input){
-		this->age;
+	masa(string name,int age){
+		this->name = name;
+		this->age  =age;
 	}
-	
-	int getage(){
-		return this->age;
-	}
-	
 };
-
+ 
 int main(){
-	
-	masa test(12);
-	
-	cout << test.getage() << endl;
-	return 0;
+    list<masa> li;
+    li.push_back(masa("ohfuji1",12));    //  後ろにデータを挿入
+    li.push_back(masa("ohfuji2",14));    //  後ろにデータを挿入
+    li.push_front(masa("ohfuji3",15));   //  前にデータを挿入
+    list<int>::iterator itr;
+    //  データの挿入
+    itr = li.begin();   //  イテレータを先頭に設定
+    itr++;              //  一つ移動
+    li.insert(itr,masa("ohfuji4",13));   //  値の挿入
+    //  データの表示
+    for (itr = li.begin(); itr != li.end(); itr++){
+        cout << *itr << " ";
+    }
+    cout << endl;
+    return 0;
 }
---
+++ excange/test1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+
+class student {
+private:
+	int age;
+	
+	
+	
+	
+	
+}
